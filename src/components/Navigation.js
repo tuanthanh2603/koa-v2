3931a8461a49c2a3b97ad5f0575da9b6ab6a752e
Hoang Phuc Hau <phuchau.tea@gmail.com>
2025-01-20 21:22:59 +0700

    fix: remove menu unused

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -68,46 +68,7 @@ export default function Navigation() {
 							"https://koastudio.vn/wp-content/uploads/1-27.jpg",
 					},
 				],
-				sections: [
-					{
-						id: "clothing",
-						name: "Clothing",
-						items: [
-							{ name: "Tops", href: "#" },
-							{ name: "Dresses", href: "#" },
-							{ name: "Pants", href: "#" },
-							{ name: "Denim", href: "#" },
-							{ name: "Sweaters", href: "#" },
-							{ name: "T-Shirts", href: "#" },
-							{ name: "Jackets", href: "#" },
-							{ name: "Activewear", href: "#" },
-							{ name: "Browse All", href: "#" },
-						],
-					},
-					{
-						id: "accessories",
-						name: "Accessories",
-						items: [
-							{ name: "Watches", href: "#" },
-							{ name: "Wallets", href: "#" },
-							{ name: "Bags", href: "#" },
-							{ name: "Sunglasses", href: "#" },
-							{ name: "Hats", href: "#" },
-							{ name: "Belts", href: "#" },
-						],
-					},
-					{
-						id: "brands",
-						name: "Brands",
-						items: [
-							{ name: "Full Nelson", href: "#" },
-							{ name: "My Way", href: "#" },
-							{ name: "Re-Arranged", href: "#" },
-							{ name: "Counterfeit", href: "#" },
-							{ name: "Significant Other", href: "#" },
-						],
-					},
-				],
+				sections: [],
 			},
 		],
 		pages: [
